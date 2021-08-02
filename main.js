d623897c342b45a752b5d325b2f2e5b4371dfaa4
Hunter-007 <bakareemmanuel571@gmail.com>
2021-08-03 00:01:12 +0100

    all files for the web quiz app in javascript n2n book

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 alert('Welcome to quiz ninja');
+const question = "What is Superman's real name?";
+const answer = prompt(question);
+alert(`You answered ${answer}`);
+/* const theAnswer = 'Clark Kent';
+const caseAnswer = toLowerCase(theAnswer);
+if (answer === theAnswer || answer === caseAnswer) {alert('You are correct!!! SUperheros Rock!!');
+}else {alert('Uh Oh! You should try again');} */
