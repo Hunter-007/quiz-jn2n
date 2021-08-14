426843df6576b405fba979b9a4319b2792cb8344
Hunter-007 <bakareemmanuel571@gmail.com>
2021-08-14 09:38:45 +0100

    Adding multiple questions and game score counter functionality

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-alert('Welcome to quiz ninja');
-const question = "What is Superman's real name?";
-const answer = prompt(question);
-alert(`You answered ${answer}`);
-/* const theAnswer = 'Clark Kent';
-const caseAnswer = toLowerCase(theAnswer);
-if (answer === theAnswer || answer === caseAnswer) {alert('You are correct!!! SUperheros Rock!!');
-}else {alert('Uh Oh! You should try again');} */
+// Set the questions
+const quiz = new Map([
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonderwoman's real name?","Dianna Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+  ]);
+
+ // initialize score
+let score = 0;
+
+for(const [question,answer] of quiz.entries()){
+
+  // get answer from user
+  const response = prompt(question);
+  // check if answer is correct
+  if(response === answer){
+    alert('Correct!');
+    // increase score by 1
+    score++;
+  } else {
+    alert(`Wrong! The correct answer was ${answer}`);
+  }
+}
+
+// At the end of the game, report the player's score
+alert(`Game Over, you scored ${score} point${score > 1 ? 's' : ''}`);
